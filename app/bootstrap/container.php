c658c11d581754f9565bd010c8c50c8c2b6eac7f
vasenin26 <leonid.vasenin@gmail.com>
2025-10-13 15:44:13 +0300

    update container

--- app/bootstrap/container.php
+++ app/bootstrap/container.php
@@ -1,20 +1,32 @@
 <?php
 
+use Anymodule\Agentmodule\Interface\ActionsFactoryInterface;
 use Anymodule\Agentmodule\Interface\ChatAgentFactoryInterface;
+use Anymodule\Agentmodule\Interface\ChatSummaryGeneratorInterface;
 use Anymodule\Agentmodule\Interface\ConversationCompressorInterface;
+use Anymodule\Agentmodule\Interface\ConversationFactoryInterface;
 use Anymodule\Agentmodule\Interface\Git\GitRepoProviderInterface;
+use Anymodule\Agentmodule\Interface\Page\PageApi;
 use Anymodule\Agentmodule\Interface\Page\PageContextServiceFactoryInterface;
 use Anymodule\Agentmodule\Interface\Task\TaskApiInterface;
 use Anymodule\Agentmodule\Interface\Task\TaskProcessorFactoryInterface;
+use Anymodule\Agentmodule\Interface\TaskStorageProviderInterface;
 use Anymodule\Agentmodule\Interface\Tools\ToolServiceFactoryInterface;
 use DI\ContainerBuilder;
 use Anymodule\Agentmodule\Factory\{
-    ActionsFactory, ConversationFactory, LLMFactory, PageContextProviderFactory,
-    TaskProcessorFactory, ToolServiceFactory
+    ActionsFactory,
+    ConversationFactory,
+    LLMFactory,
+    PageContextProviderFactory,
+    TaskProcessorFactory,
+    ToolServiceFactory
 };
 use Anymodule\Agentmodule\Services\{
-    ApiService\Service, RepositoryService\RepositoryProvider,
-    Summary\SummaryCompressor, Summary\SummaryGenerator, TaskStorageProvider
+    ApiService\DocModuleApi,
+    RepositoryService\RepositoryProvider,
+    Summary\SummaryCompressor,
+    Summary\SummaryGenerator,
+    TaskStorageProvider
 };
 use Anymodule\Agentmodule\Services\ModelsDirectory\ModelsProvider;
 use Anymodule\Agentmodule\Utils\BrokenCompressor;
@@ -22,7 +34,7 @@ use Anymodule\Agentmodule\Utils\BrokenCompressor;
 $builder = new ContainerBuilder();
 $builder->addDefinitions([
 
-    Service::class => fn() => new Service(
+    DocModuleApi::class => fn() => new DocModuleApi(
         host: getenv('API_HOST'),
         token: getenv('API_TOKEN') ?: getenv('AGENT_TOKEN'),
     ),
@@ -32,12 +44,8 @@ $builder->addDefinitions([
         reposFolder: getenv('REPOS_FOLDER') ?: 'default'
     ),
 
-    ModelsProvider::class => fn() => new ModelsProvider(),
-
-    ToolServiceFactory::class => fn($c) => new ToolServiceFactory(
-        $c->get(GitRepoProviderInterface::class),
-        new PageContextProviderFactory($c->get(Service::class)),
-    ),
+    ModelsProvider::class => DI\autowire(ModelsProvider::class),
+    ToolServiceFactory::class => DI\autowire(ToolServiceFactory::class),
 
     SummaryCompressor::class => fn($c) => new SummaryCompressor(
         new SummaryGenerator(
@@ -50,21 +58,20 @@ $builder->addDefinitions([
         )
     ),
 
-    LLMFactory::class => fn($c) => new LLMFactory(
-        $c->get(GitRepoProviderInterface::class),
-        $c->get(SummaryCompressor::class),
-        $c->get(ModelsProvider::class)
-    ),
+    TaskProcessorFactoryInterface::class => DI\autowire(TaskProcessorFactory::class),
 
-    TaskProcessorFactoryInterface::class => fn($c) => new TaskProcessorFactory(
-        $c->get(ToolServiceFactory::class),
-        $c->get(LLMFactory::class),
-        new ConversationFactory(),
-        new TaskStorageProvider(),
-        new ActionsFactory($c->get(ToolServiceFactory::class), $c->get(LLMFactory::class)),
-    ),
+    ToolServiceFactoryInterface::class => DI\autowire(ToolServiceFactory::class),
+    ChatAgentFactoryInterface::class => DI\autowire(LLMFactory::class),
+    ConversationFactoryInterface::class => DI\autowire(ConversationFactory::class),
+    TaskStorageProviderInterface::class => DI\autowire(TaskStorageProvider::class),
+    ActionsFactoryInterface::class => DI\autowire(ActionsFactory::class),
+    PageContextServiceFactoryInterface::class => DI\autowire(PageContextProviderFactory::class),
 
-    TaskApiInterface::class => fn($c) => $c->get(Service::class),
+    PageApi::class => fn($c) => $c->get(DocModuleApi::class),
+    TaskApiInterface::class => fn($c) => $c->get(DocModuleApi::class),
+
+    ConversationCompressorInterface::class => fn($c) => $c->get(SummaryCompressor::class),
+    ChatSummaryGeneratorInterface::class => DI\autowire(SummaryGenerator::class),
 
 ]);
 
