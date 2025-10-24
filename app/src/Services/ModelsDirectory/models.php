9f659a748741a76bce2347b60891b923ac7785d0
vasenin26 <leonid.vasenin@gmail.com>
2025-10-24 10:53:07 +0300

    Update models.php to include host configuration for various models, enhancing server integration

--- app/src/Services/ModelsDirectory/models.php
+++ app/src/Services/ModelsDirectory/models.php
@@ -1,124 +1,157 @@
 <?php
 
+$intenalServer = '';
+$openaiServer = '';
+
 return [
     'llm-studio' => [
         'name' => 'llm-studio',
-        'context' => 15000
+        'context' => 15000,
+        'host' => $intenalServer,
     ],
     'summary' => [
         'name' => 'gpt-4.1',
-        'context' => 1_047_576
+        'context' => 1_047_576,
+        'host' => $openaiServer,
     ],
     'gpt-5' => [
         'name' => 'gpt-5',
-        'context' => 150_000
+        'context' => 150_000,
+        'host' => $openaiServer,
     ],
     'gpt-5-mini' => [
         'name' => 'gpt-5-mini',
         'context' => 70_000,
+        'host' => $openaiServer,
     ],
     'gpt-5-nano' => [
         'name' => 'gpt-5-nano',
         'context' => 70_000,
+        'host' => $openaiServer,
     ],
     'gpt-4.1' => [
         'name' => 'gpt-4.1',
         'context' => 1_047_576,
+        'host' => $openaiServer,
     ],
     'gpt-4.1-nano' => [
         'name' => 'gpt-4.1-nano',
         'context' => 1_047_576,
+        'host' => $openaiServer,
     ],
     'gpt-5-codex' => [
         'name' => 'gpt-5-codex',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-5-pro' => [
         'name' => 'gpt-5-pro',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4.1-mini' => [
         'name' => 'gpt-4.1-mini',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o' => [
         'name' => 'gpt-4o',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o-2024-05-13' => [
         'name' => 'gpt-4o-2024-05-13',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o-mini' => [
         'name' => 'gpt-4o-mini',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-realtime' => [
         'name' => 'gpt-realtime',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-realtime-mini' => [
         'name' => 'gpt-realtime-mini',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o-realtime-preview' => [
         'name' => 'gpt-4o-realtime-preview',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o-mini-realtime-preview' => [
         'name' => 'gpt-4o-mini-realtime-preview',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o1' => [
         'name' => 'o1',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o1-pro' => [
         'name' => 'o1-pro',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o3-pro' => [
         'name' => 'o3-pro',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o3' => [
         'name' => 'o3',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o3-deep-research' => [
         'name' => 'o3-deep-research',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o4-mini' => [
         'name' => 'o4-mini',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o4-mini-deep-research' => [
         'name' => 'o4-mini-deep-research',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o3-mini' => [
         'name' => 'o3-mini',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'o1-mini' => [
         'name' => 'o1-mini',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'codex-mini-latest' => [
         'name' => 'codex-mini-latest',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-5-search-api' => [
         'name' => 'gpt-5-search-api',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o-mini-search-preview' => [
         'name' => 'gpt-4o-mini-search-preview',
         'context' => 90_000,
+        'host' => $openaiServer,
     ],
     'gpt-4o-search-preview' => [
         'name' => 'gpt-4o-search-preview',
         'context' => 90_000,
+        'host' => $openaiServer,
     ]
 ];
