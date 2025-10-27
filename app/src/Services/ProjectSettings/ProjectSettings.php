592c5743a1524f804b144d911db1d3822b4bf96e
vasenin26 <leonid.vasenin@gmail.com>
2025-10-27 21:58:34 +0300

    add load models log

--- app/src/Services/ProjectSettings/ProjectSettings.php
+++ app/src/Services/ProjectSettings/ProjectSettings.php
@@ -4,6 +4,7 @@ namespace Anymodule\Agentmodule\Services\ProjectSettings;
 
 use Anymodule\Agentmodule\Interface\ProjectApiInterface;
 use Anymodule\Agentmodule\Interface\Storage\ProjectSettingsInterface;
+use Anymodule\Agentmodule\Utils\Log;
 
 class ProjectSettings implements ProjectSettingsInterface
 {
@@ -20,6 +21,7 @@ class ProjectSettings implements ProjectSettingsInterface
     {
         if (empty($this->preferredModels)) {
             $this->preferredModels = $this->projectApi->getPreferredModels($this->projectId);
+            Log::info("Loaded project {$this->projectId} preferred models", $this->preferredModels);
         }
 
         return $this->preferredModels[$taskType] ?? null;
